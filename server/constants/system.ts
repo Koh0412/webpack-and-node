835f78680b4afbfc12ec3b6ccf3ddb8de4fa754c
kohei.hujita <point.out92@gmail.com>
2020-09-23 16:06:02 +0900

    fix logger

--- server/constants/system.ts
+++ server/constants/system.ts
@@ -2,8 +2,8 @@ import * as log4js from "log4js";
 
 export const PORT_NUMBER = 8011;
 
-export const CONTENT_TYPE_HTML = "text/html";
-export const DEFAULT_CHARSET = "charset=utf-8"
+export const DEFAULT_CHARSET = "charset=utf-8";
+export const CONTENT_TYPE_HTML = "text/html; " + DEFAULT_CHARSET;
 export const ROOT = "/";
 export const EXT_HTML = ".html";
 
